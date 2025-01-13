682ff407e1e2e00d11681436dbf87b4d0e8bea40
homoki-denis <denis.homoki6@gmail.com>
2025-01-13 21:52:14 +0200

    chore: project almost done

--- index.js
+++ index.js
@@ -1,24 +1,49 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
+
+// express.json() and express.urlencoded() are built-in middleware
+//  functions to support JSON-encoded and URL-encoded bodies.
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+
+let urlMapping = {};
+
+app.post("/api/shorturl", function (req, res) {
+  let originalUrl = req.body.url;
+  let shortUrl = Math.floor(Math.random() * 1000);
+
+  try {
+    new URL(originalUrl);
+
+    urlMapping[shortUrl] = originalUrl;
+
+    return res.json({ original_url: originalUrl, short_url: shortUrl });
+  } catch (e) {
+    return res.json({ error: "invalid url" });
+  }
 });
 
-app.listen(port, function() {
+app.get("/api/shorturl/:shortLink", (req, res) => {
+  let shortLink = req.params.shortLink;
+
+  res.redirect(urlMapping[shortLink]);
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
