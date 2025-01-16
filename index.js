c65b4c6c25c8c98cf27a2696f269eeff58834961
homoki-denis <denis.homoki6@gmail.com>
2025-01-16 20:33:28 +0200

    chore: finish project

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const dns = require("dns");
+
 const app = express();
+
+const options = {
+  // Setting family as 6 i.e. IPv6
+  family: 6,
+  hints: dns.ADDRCONFIG | dns.V4MAPPED,
+};
 
 // express.json() and express.urlencoded() are built-in middleware
 //  functions to support JSON-encoded and URL-encoded bodies.
@@ -27,15 +35,17 @@ app.post("/api/shorturl", function (req, res) {
   let originalUrl = req.body.url;
   let shortUrl = Math.floor(Math.random() * 1000);
 
-  try {
-    new URL(originalUrl);
+  let domain = new URL(originalUrl).hostname;
+
+  dns.lookup(domain, options, (err, address, family) => {
+    if (err) {
+      return res.json({ error: "invalid url" });
+    }
 
     urlMapping[shortUrl] = originalUrl;
 
     return res.json({ original_url: originalUrl, short_url: shortUrl });
-  } catch (e) {
-    return res.json({ error: "invalid url" });
-  }
+  });
 });
 
 app.get("/api/shorturl/:shortLink", (req, res) => {
